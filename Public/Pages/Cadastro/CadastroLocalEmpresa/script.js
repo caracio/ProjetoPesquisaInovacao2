ce8c19d3c3542cbec9a03ac94ddbcf897f094041
rogerio-alvesf <rogerio.filho@bandtec.com.br>
2022-03-06 21:59:45 -0300

    Criação de todas as telas de cadastro já responsivas.

---
+++ Public/Pages/Cadastro/CadastroLocalEmpresa/script.js
@@ -0,0 +1,15 @@
+/*Declara a viravel hamburguer que quando é clicado chama uma função
+que pega o container e com a lista de classes altera ou adiciona com o
+toggle uma classe chamada show que muda o comportamento da sidebar
+*/
+
+var hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu")
+});
+
+/* OU usando arrow function
+document.querySelector(".hamburguer").addEventListener("click", () =>
+      document.querySelector(".container").classList.toggle("show-menu")
+);*/
