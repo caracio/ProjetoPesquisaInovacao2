25a8a8ca10d12449728fe2c63022602a69ec73d5
victor077 <victor.souza@bandtec.com.br>
2022-03-27 21:59:04 -0300

    tela de cadastro na modal

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'xxxxxxxxxx', //Senha so seu usuario root no database
+    password: 'Banho009', //Senha so seu usuario root no database
     database: 'farmagraphsolutions'  
 });
 
