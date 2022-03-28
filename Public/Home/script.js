0798006fbbff20a1cee62c86adc59a3d718b944c
victor077 <victor.souza@bandtec.com.br>
2022-03-28 11:32:01 -0300

    colocando os icones e termos de politica

--- Public/Home/script.js
+++ Public/Home/script.js
@@ -1,4 +1,3 @@
-
 var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", function () {
@@ -13,3 +12,21 @@ function fecharModal() {
   document.getElementById("modal").style.top = "-100%";
 }
 
+let btnOlho = document.querySelector('.fa-eye');
+
+btnOlho.addEventListener('click', () => {
+  let inputSenha = document.querySelector('#in_Senha');
+  let inputConfirmaSenha = document.querySelector('#in_confirmar_Senha');
+
+  if (inputSenha.getAttribute('type') == 'password') {
+    inputSenha.setAttribute('type', 'text');
+  } else {
+    inputSenha.setAttribute('type', 'password');
+  }
+  
+  if (inputConfirmaSenha.getAttribute('type') == 'password') {
+    inputConfirmaSenha.setAttribute('type', 'text');
+  } else {
+    inputConfirmaSenha.setAttribute('type', 'password');
+  }
+});
