3f3ae744fc4c8e3e4d1eb7b1fe1be7466cf2c120
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-04-10 13:28:37 -0300

    Tela de cadastro pegando dados do formulario e redirecionando para a DashBoard

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Banho009', //Senha so seu usuario root no database
+    password: 'abaxiiionivurh90hr', //Senha so seu usuario root no database
     database: 'farmagraphsolutions'  
 });
 
--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,7 +1,7 @@
 class Tabelas {
   init(conexao) {
     this.conexao = conexao;
-    this.RedeFarmacia();
+    this.Responsavel();
     this.Loja();
     this.Monitorador();
     this.Computador(); 
@@ -9,20 +9,19 @@ class Tabelas {
     this.LOGS_Monitoramento();
   }
   //projetoPiFarmacia --> Criação das Tabelas no Banco de Dados
-  RedeFarmacia() {
+  Responsavel() {
     const sql = `
-    CREATE TABLE IF NOT EXISTS RedeFarmacia(ID_RedeFarmacia 	Integer PRIMARY KEY AUTO_INCREMENT
-                                           ,NM_Rede		  	    VARCHAR(40)  NOT NULL
-                                           ,Email	    		  	VARCHAR(40)  NOT NULL
-                                           ,Senha      		  	VARCHAR(40)  NOT NULL
-
+    CREATE TABLE if not exists  Responsavel(ID_Responsavel 	Integer PRIMARY KEY AUTO_INCREMENT
+                                           ,NM_Rede		  	  VARCHAR(40)  NOT NULL
+                                           ,Email		  	  	VARCHAR(40)  NOT NULL
+                                           ,Senha 		    	VARCHAR(40)  NOT NULL
                  )AUTO_INCREMENT = 1;
-        `;
+`;
     this.conexao.query(sql, (error) => {
       if (error) {
-        throw new Error("Erro ao Criar Tabela RedeFarmacia -->" + error);
+        throw new Error("Erro ao Criar Tabela Responsavel -->" + error);
       }
-      console.log("Tabela RedeFarmacia criada com sucesso!!!\n");
+      console.log("Tabela Responsavel criada com sucesso!!!\n");
     });
   }
   Monitorador() {
@@ -49,27 +48,30 @@ class Tabelas {
   }
   Loja() {
     const sql = `
-    CREATE TABLE IF NOT EXISTS Loja (ID_Loja 		 	    Integer PRIMARY KEY AUTO_INCREMENT
-                                    ,Nome 	  		    VARCHAR(45) NOT NULL
-                                    ,Rua 	  			    VARCHAR(40) NOT NULL
-                                    ,Bairro  			    VARCHAR(30) NOT NULL
-                                    ,Numero  			    SMALLINT  	NOT NULL
-                                    ,Cidade 			    VARCHAR(45) NOT NULL
-                                    ,CNPJ 			      CHAR(14)	  NOT NULL
-                                    ,FK_RedeFarmacia  Integer 
-                                    ,FK_Filial        Integer
-                                            
-                            ,CONSTRAINT FK_RedeFarmacia_Loja 
-                                        FOREIGN KEY (FK_Redefarmacia)	
-                                                    REFERENCES RedeFarmacia(ID_RedeFarmacia)
-                                        
-                            ,CONSTRAINT FK_Filial_Loja 
-                                        FOREIGN KEY (FK_Filial)	
-                                                    REFERENCES Loja(ID_Loja)                                          						
-              ); 
+    CREATE TABLE if not exists Loja (ID_Loja 		 	      Integer PRIMARY KEY AUTO_INCREMENT
+                                    ,Nome 				      VARCHAR(70) NOT NULL
+                                    ,Rua 				        VARCHAR(40) NOT NULL
+                                    ,Bairro  			      VARCHAR(30) NOT NULL
+                                    ,Numero  			      SMALLINT 	  NOT NULL
+                                    ,Cidade 			      VARCHAR(45) NOT NULL
+                                    ,Estado				      CHAR(2)     NOT NULL
+                                    ,CEP  				      CHAR(9)     NOT NULL
+                                    ,CNPJ 				      CHAR(14)	  NOT NULL
+                                    ,FK_Responsavel   	Integer 
+                                    ,FK_Filial        	Integer
+      
+        ,CONSTRAINT FK_Responsavel_Loja 
+              FOREIGN KEY (FK_Responsavel)	
+                    REFERENCES Responsavel(ID_Responsavel)
+                    
+        ,CONSTRAINT FK_Filial_Loja 
+              FOREIGN KEY (FK_Filial)	
+                    REFERENCES Loja(ID_Loja)                                          						
+        );
   
 
   `;
+  
     this.conexao.query(sql, (error) => {
       if (error) {
         throw new Error("Erro ao Criar Tabela Loja --->" + error);
@@ -120,17 +122,17 @@ class Tabelas {
   }
   Monitorador() {
     const sql = `
-    CREATE TABLE IF NOT EXISTS Monitorador (ID_Monitorador 	Integer PRIMARY KEY AUTO_INCREMENT
-                                           ,FK_Loja 		 	  Integer 
-                                           ,Nome 			  	  VARCHAR(40) NOT NULL
-                                           ,CPF 			 	    CHAR(11)	NOT NULL
-                                           ,Email			 	    VARCHAR(40) NOT NULL
-                                           ,Senha 		 	    VARCHAR(40) NOT NULL
-   
-               ,CONSTRAINT FK_Loja_Monitorador
-                           FOREIGN KEY (FK_Loja)
-                                       REFERENCES Loja(ID_Loja)
-                  )AUTO_INCREMENT = 1;
+    CREATE TABLE IF NOT EXISTS Monitorador (ID_Monitorador 		Integer PRIMARY KEY AUTO_INCREMENT
+                                           ,FK_Loja 	    	 	Integer 
+                                           ,Nome 			      	VARCHAR(40) NOT NULL
+                                           ,CPF   			     	CHAR(11)	NOT NULL
+                                           ,Email			    	  VARCHAR(40) NOT NULL
+                                           ,Senha 		    		VARCHAR(40) NOT NULL
+ 
+                                          ,CONSTRAINT FK_Loja_Monitorador
+                                                FOREIGN KEY (FK_Loja)
+                                                      REFERENCES Loja(ID_Loja)
+                                              )AUTO_INCREMENT = 1;
 
                         
         `;
@@ -146,14 +148,14 @@ class Tabelas {
     const sql = `
     CREATE TABLE IF NOT EXISTS LOGS_Monitoramento(ID_LogsMonitoramento  	INTEGER PRIMARY KEY AUTO_INCREMENT
                                                  ,FrequenciaProcessador 	VARCHAR(45) 
-                                                 ,UsoMemoriaRAM			    	VARCHAR(45) 
-                                                 ,QTD_LeituraDisco   			VARCHAR(45) 
-                                                 ,QTD_LeituraSSD    			VARCHAR(45) 
+                                                 ,UsoMemoriaRAM			  	  VARCHAR(45) 
+                                                 ,QTD_LeituraDisco 			  VARCHAR(45) 
+                                                 ,QTD_LeituraSSD			    VARCHAR(45) 
                                                  ,FK_Componentes  	    	Integer
               
                                                 ,CONSTRAINT FK_Componentes_LOG
-                                                            FOREIGN KEY (FK_Componentes)                  
-                                                                        REFERENCES Componentes (ID_Componentes)
+                                                      FOREIGN KEY (FK_Componentes)
+                                                          REFERENCES Componentes (ID_Componentes)
               );
                 `;
 
