382dddac3ffd62ff7a73b8bf8130cad97236dd03
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-04-24 01:18:18 -0300

    Mudança no packJson -- npm i

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const conexao = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '@Planeta2011', //Senha so seu usuario root no database
+    password: 'xxxxxxxx', //Senha so seu usuario root no database
     database: 'farmagraphsolutions'  
 });
 
