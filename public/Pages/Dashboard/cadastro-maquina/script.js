ea9fd0d0a65b4697c6c3820cdb96a233c4477fa7
rogerio-alvesf <rogerio.filho@bandtec.com.br>
2022-05-15 16:43:17 -0300

    Correção de bug nos formularios e ajuste na home da dashboard.

--- public/Pages/Dashboard/cadastro-maquina/script.js
+++ public/Pages/Dashboard/cadastro-maquina/script.js
@@ -2,3 +2,21 @@ function sair() {
     sessionStorage.clear();
     window.location = "../../../index.html";
 }
+
+const informacoesUsuario = JSON.parse(sessionStorage.getItem("res"));
+
+document.getElementById("nomeAdmin").innerHTML = informacoesUsuario.nome;
+
+const dados = JSON.parse(sessionStorage.getItem("res")).lojas;
+
+function carregarLojas() {
+    var opcoesLoja = document.getElementById('lojasExistentes');
+
+    for (let i = 0; i < dados.length; i++) {
+        var option = document.createElement("option");
+        option.text = dados[i].nome;
+        console.log(dados[i].nome);
+        opcoesLoja.add(option);
+    }
+}
+carregarLojas();
--- public/Pages/Dashboard/dashboard-charts/dashboard.js
+++ public/Pages/Dashboard/dashboard-charts/dashboard.js
@@ -13,20 +13,24 @@
   }
 })();
 
+//ARRUMAR
 const informacoesUsuario = JSON.parse(sessionStorage.getItem("res"));
+console.log(informacoesUsuario.lojas[0].computadores);
 
 function carregarMaquina() {
-    var opcoesMaquina = document.getElementById('maquinasExistentes');
+  var opcoesMaquina = document.getElementById('maquinasExistentes');
+  var option = document.createElement("option");
+  for (let i = 0; i < informacoesUsuario.lojas[0].computadores.lenght; i++) {
+    option.text = informacoesUsuario.lojas[0].computadores[i].ID_Computador;
+    opcoesMaquina.add(option);
+  }
 
-    for (let i = 0; i < dados.length; i++) {
-        var option = document.createElement("option");
-        option.text = dados[i].nome;
-        opcoesMaquina.add(option);
-    }
+  option.text = informacoesUsuario.loja[JSON.parse(sessionStorage.getItem("idLoja"))].computadores;
+  opcoesMaquina.add(option);
 }
 carregarMaquina();
 
-function sair(){
+function sair() {
   sessionStorage.clear();
   window.location = "../../../index.html";
 }
