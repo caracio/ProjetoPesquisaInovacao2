f71e31fe924f1416d362c0f5b7f86495bd20e744
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-05-21 14:18:46 -0300

    Data of logs in azure

--- src/models/logsMemoriaMassaModels.js
+++ src/models/logsMemoriaMassaModels.js
@@ -1,4 +1,5 @@
 const {conexao} = require("../infraestrutura/conexao");
+const { conexaoAzure, sql } = require("../infraestrutura/conexaoAzure");
 
 class LogsMemoriaMassaModels {
   getLogsMonitoramento(req, res) {
@@ -12,6 +13,29 @@ class LogsMemoriaMassaModels {
 
         res.status(200).json(results[0]);
     })
+
+  }
+  async getLogsMonitoramentoAzure(req, res){
+    const { idComputador, idLoja }= req;
+
+    const pool = await conexaoAzure();
+
+    pool
+    .request()
+    .input('ID_Computador', idComputador)
+    .input('FK_Loja', idLoja)
+    .output('EspacoArmazenado', sql.BigInt)
+    .output('EspacoLivre', sql.BigInt)
+    .execute(
+      'dbo.SP_LogsMemoriaMassa', async(error, results)=>{ 
+        if (error) {
+          return await res
+            .status(500)
+            .json({ ...error, Message: "Erro no servidor..." });
+        }
+        await res.status(200).json(results.output);
+      }
+    )
   }
 }
 
--- src/models/logsMemoriaRamModels.js
+++ src/models/logsMemoriaRamModels.js
@@ -1,18 +1,43 @@
-const {conexao} = require("../infraestrutura/conexao");
+const { conexao } = require("../infraestrutura/conexao");
+const { conexaoAzure, sql } = require("../infraestrutura/conexaoAzure");
 
 class LogsMemoriaRamModels {
   getLogsMonitoramento(req, res) {
     const sql = `CALL SP_DadosMemoriaRam(?,?,@_memoriaUso);`;
 
-    conexao.query(sql, [req.idComputador,req.idLoja], async(error,results)=>{
-        if(error){
-            await res.status(500).json(results);
-            return;
+    conexao.query(
+      sql,
+      [req.idComputador, req.idLoja],
+      async (error, results) => {
+        if (error) {
+          await res.status(500).json(results);
+          return;
         }
 
-       await res.status(200).json(results[0]);
+        await res.status(200).json(results[0]);
+      }
+    );
+  }
 
-    })
+  async getLogsMonitoramentoAzure(req, res) {
+    const { idComputador, idLoja } = req;
+
+    const pool = await conexaoAzure();
+
+    pool
+      .request()
+      .input("ID_Computador", idComputador)
+      .input("ID_Loja", idLoja)
+      .output("memoriaUso", sql.BigInt)
+      .execute("dbo.SP_LogsMemoriaRam", async (error, results) => {
+        if (error) {
+          return await res
+            .status(500)
+            .json({ ...error, Message: "Erro no servidor..." });
+        }
+        
+        await res.status(200).json(results.output);
+      });
   }
 }
 
